e68fac401d65cfdd2f816f64290a8ef390fcc419
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-02-03 12:04:36 +0500

    tasks

--- bouncing ball animation/js_lab.js
+++ bouncing ball animation/js_lab.js
@@ -1,6 +1,59 @@
-console.log(age)
-var age = "2"
-var age2 = 2
-console.log(age == age2)
+// console.log(age)
+var age = 2;
+// var age2 = 2
+// console.log(age == age2)
 
-console.log(age === age2)
+// console.log(age === age2)
+
+//single alternative
+
+// if(age > 1)
+// {
+//     console.log("Age is greater than 1")
+// }
+
+//double alternative
+
+// if(age > 1)
+// {
+//     console.log("Age is greater than 1")
+// }
+// else
+// {
+//     console.log("Age is less than 1")
+// }
+
+// (age > 1) ? True: False;
+
+// ternary operator
+// if (typeof age === "number") {
+//   age > 1
+//     ? console.log("Age is greater than 1")
+//     : console.log("Age is less than 1");
+// } else {
+//   console.log("Age is not defined or not a number");
+// }
+
+// switch case
+
+// switch (age) {
+//   case 1:
+//     console.log("Age is 1");
+//     break;
+//   case 2:
+//     console.log("Age is 2");
+//     break;
+//   default:
+//     console.log("Age is not 1 or 2");
+//     break;
+// }
+
+// multiple alternative using nested if else
+
+if (age > 1 && age <= 2) {
+  console.log("Age is greater than 1");
+} else if (age == 2) {
+  console.log("Age is less than 2");
+} else {
+  console.log("Age is not 1 or 2");
+}
---
+++ bouncing ball animation/objectsandarrays.js
@@ -0,0 +1,22 @@
+// var clothes = [];
+
+// clothes.push("shirt");
+// clothes.push("pants");
+// clothes.push("jacket");
+// clothes.push("socks");
+// clothes.push("shoes");
+
+
+
+// //removing fifth element
+
+// clothes.pop();
+// console.log(clothes);
+
+
+var favCar = {}
+
+favCar.color = "red";
+favCar.convertable = true;
+
+console.log(favCar);
